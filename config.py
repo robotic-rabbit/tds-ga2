9aeb8b6aab430f299760fd0d40d43fdb912c9944
23f2003412 <23f2003412@ds.study.iitm.ac.in>
2026-07-07 18:32:03 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,43 @@
+# ==========================================
+# MASTER CONFIGURATION
+# ==========================================
+
+# Your IITM Email
+EMAIL = "23f2003412@ds.study.iitm.ac.in"
+
+# Q1
+Q1_ALLOWED_ORIGIN = "https://dash-hr26mq.example.com"
+
+# Q2
+ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-jyecho56.apps.exam.local"
+
+PUBLIC_KEY_PEM = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
+
+# Q3
+Q3_PORT = 8171
+Q3_WORKERS = 7
+Q3_DEBUG = False
+Q3_LOG_LEVEL = "error"
+
+# Q5
+Q5_API_KEY = "ak_pxdpvmqdj6ne3wxdmj9rus4k"
+
+# Q9
+Q9_TOTAL_ORDERS = 42
+Q9_RATE_LIMIT = 15
+
+# Q10
+Q10_ALLOWED_ORIGIN = "https://app-1s97ml.example.com"
+Q10_RATE_LIMIT = 13
+
+# Fixed
+EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
